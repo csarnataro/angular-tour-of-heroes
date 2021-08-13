c9287a28f19491e25806301313e29c80f3f6db27
Christian Sarnataro <c.sarnataro@arduino.cc>
2021-08-13 16:57:27 +0200

    Initial import

---
+++ src/app/mock.users.ts
@@ -0,0 +1,28 @@
+// use https://randomuser.me/api/?results=10 instead
+
+import { Hero } from "~/models/hero";
+
+export const HEROES: Hero[] = [{
+  id: 1,
+  name: 'Lilly'
+},
+{
+  id: 2,
+  name: 'Hocus'
+},
+{
+  id: 3,
+  name: 'Pepe'
+},
+{
+  id: 4,
+  name: 'Gino'
+},
+{
+  id: 5,
+  name: 'Pino'
+},
+{
+  id: 6,
+  name: 'Ant'
+}]
---
+++ src/app/services/hero-service/hero.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { Hero } from '~/models/hero';
+import { HEROES } from '~/mock.users';
+import { Observable, of } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
+import { MessageService } from '~/services/message-service/message.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+/* https://randomuser.me/api/?results=10 */
+export class HeroService {
+
+  private heroesUrl = 'https://randomuser.me/api/?results=10&seed=7b03ffa8d1c50276'
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  getHeroes(): Observable<any> {
+    return this.http.get<any>(this.heroesUrl).pipe(
+      // map(topLevelObjects => {
+      //   console.log('******** BEGIN: hero.service:23 ********');
+      //   console.dir(topLevelObjects, { depth: null, colors: true });
+      //   console.log('********   END: hero.service:23 ********');
+      //   return topLevelObjects.results
+      // }),
+      pluck('results'),
+      map(results => {
+        console.log('******** BEGIN: hero.service:30 ********');
+        console.dir(results.length, { depth: null, colors: true });
+        console.log('********   END: hero.service:30 ********');
+        return results; // { id: Math.floor(Math.random()*10), name: };
+        // return { id: Math.floor(Math.random()*10), name: };
+      }),
+      map(single => {
+        console.log('******** BEGIN: hero.service:37 ********');
+        console.dir(single, { depth: null, colors: true });
+        console.log('********   END: hero.service:37 ********');
+        return ({ id: Math.floor(Math.random()*10), name: Math.floor(Math.random()*10) })}
+    ));
+
+
+    // this.messageService.add("Heroes fetched")
+    // return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
+  }
+
+
+}
