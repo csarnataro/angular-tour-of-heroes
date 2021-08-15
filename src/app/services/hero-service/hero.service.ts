3a02801b4c223c3a0d0e33af15a62a6a7c5ca7f6
Christian Sarnataro <csarnataro@users.noreply.github.com>
2021-08-15 12:06:09 +0200

    Updated HeroService to use rxjs' map

--- src/app/services/hero-service/hero.service.ts
+++ src/app/services/hero-service/hero.service.ts
@@ -1,8 +1,8 @@
 import { Observable, of } from 'rxjs';
-import { map, pluck } from 'rxjs/operators';
+import { map, pluck, mergeMap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Hero } from '../../models/hero';
+import { Hero } from '../../models/hero';
 import { HEROES } from '../../mock.users';
 import { MessageService } from '../../services/message-service/message.service';
 
@@ -11,38 +11,30 @@ import { MessageService } from '../../services/message-service/message.service';
 })
 /* https://randomuser.me/api/?results=10 */
 export class HeroService {
-
-  private heroesUrl = 'https://randomuser.me/api/?results=10&seed=7b03ffa8d1c50276'
+  private heroesUrl =
+    'https://randomuser.me/api/?results=10&seed=7b03ffa8d1c50276';
   constructor(
     private http: HttpClient,
-    private messageService: MessageService) { }
+    private messageService: MessageService
+  ) {}
 
   getHeroes(): Observable<any> {
+    this.messageService.add('Fetching heroes');
     return this.http.get<any>(this.heroesUrl).pipe(
-      // map(topLevelObjects => {
-      //   console.log('******** BEGIN: hero.service:23 ********');
-      //   console.dir(topLevelObjects, { depth: null, colors: true });
-      //   console.log('********   END: hero.service:23 ********');
-      //   return topLevelObjects.results
-      // }),
-      pluck('results'),
-      map(results => {
-        console.log('******** BEGIN: hero.service:30 ********');
-        console.dir(results.length, { depth: null, colors: true });
-        console.log('********   END: hero.service:30 ********');
-        return results; // { id: Math.floor(Math.random()*10), name: };
-        // return { id: Math.floor(Math.random()*10), name: };
-      }),
-      map(single => {
-        console.log('******** BEGIN: hero.service:37 ********');
-        console.dir(single, { depth: null, colors: true });
-        console.log('********   END: hero.service:37 ********');
-        return ({ id: Math.floor(Math.random()*10), name: Math.floor(Math.random()*10) })}
-    ));
-
-
-    // this.messageService.add("Heroes fetched")
-    // return of(HEROES);
+      // pluck('results'),
+      map(json => json.results),
+      map(results =>
+        results.map(single => {
+          console.log('******** BEGIN: hero.service:30 ********');
+          console.log(single);
+          console.log('********   END: hero.service:30 ********');
+          return {
+            id: Math.floor(Math.random() * 10),
+            name: `${single.name.first} ${single.name.last}`
+          };
+        })
+      )
+    );
   }
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
@@ -53,8 +45,6 @@ export class HeroService {
   }
 
   private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`)
+    this.messageService.add(`HeroService: ${message}`);
   }
-
-
 }
